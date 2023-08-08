2e82d73cdd976005be05b6f251075ea9e7237a99
Eya HajAyed <eyahadjayed@gmail.com>
2023-08-08 01:57:29 +0100

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@GetMapping("message")
 	public String getMessage(){
 		return "love you eya ";
 	}
